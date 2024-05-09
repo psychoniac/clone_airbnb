29645c274ea169c64c13b11df2e2a0ed0565710f
jlnko <jlnko@hotmail.fr>
2024-05-09 17:34:32 +0200

    création du composants search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Nav from "../components/Nav";
+import Search from "../components/Search";
 
 
 export default function Home() {
   return (
     <div>
       <Nav />
+      <Search />
     </div>
   )
 }
